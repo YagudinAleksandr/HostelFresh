9585137d95ca9153e1f0aae34814d7b29c180bda
Александр <@USER>
2024-12-27 21:50:20 +0300

    Добавьте файлы проекта.

---
+++ Application/HostelFresh.Application.Abstractions/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace HostelFresh.Application.Abstractions.Entities
+{
+    /// <summary>
+    /// Базовая сущность
+    /// </summary>
+    /// <typeparam name="T">Тип ИД</typeparam>
+    public interface IEntity<T>
+    {
+        T Id { get; set; }
+    }
+}
