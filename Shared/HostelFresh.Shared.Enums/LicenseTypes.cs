e5161ff46cfd775df768dc17fb7681b96332612e
Александр <@USER>
2024-12-29 13:54:15 +0300

    #2 Добавление перечислений с типами

---
+++ Shared/HostelFresh.Shared.Enums/LicenseTypes.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HostelFresh.Shared.Enums
+{
+    /// <summary>
+    /// Типы лицензий
+    /// </summary>
+    public enum LicenseTypes
+    {
+        /// <summary>
+        /// Лицензия
+        /// </summary>
+        [Display(Name = "Лицензия")]
+        License = 1,
+
+        /// <summary>
+        /// Доверенность
+        /// </summary>
+        [Display(Name = "Доверенность")]
+        Attorney = 2
+    }
+}
