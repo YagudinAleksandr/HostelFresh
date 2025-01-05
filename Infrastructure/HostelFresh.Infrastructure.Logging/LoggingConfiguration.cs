65480d27ce1a5b44a71b204c62cf44c8a7d3f522
Александр <@USER>
2025-01-05 15:45:09 +0300

    #4 добавление логирования

---
+++ Infrastructure/HostelFresh.Infrastructure.Logging/LoggingConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace HostelFresh.Infrastructure.Logging
+{
+    /// <summary>
+    /// Кастомная настройка логирования
+    /// </summary>
+    public static class LoggingConfiguration
+    {
+        /// <summary>
+        /// Добавление логирования
+        /// </summary>
+        /// <param name="services">Коллекция сервисов</param>
+        /// <param name="configuration">Настройки</param>
+        public static void AddCustomLogging(this IServiceCollection services, IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration) 
+                .CreateLogger();
+
+            services.AddLogging(loggingBuilder =>
+                loggingBuilder.AddSerilog(dispose: true));
+        }
+    }
+}
