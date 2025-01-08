d0b7b86d6394171a94665160c343235ad46d8e21
Александр <@USER>
2025-01-08 13:16:30 +0300

    #5 Добавлены настройки фабрики Redis

---
+++ Application/HostelFresh.Application.Abstractions/Factories/IRedisFactory.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+namespace HostelFresh.Application.Abstractions.Factories
+{
+    /// <summary>
+    /// Фабрика подключения к Redis
+    /// </summary>
+    public interface IRedisFactory
+    {
+        /// <summary>
+        /// Создание контекста подключения к Redis
+        /// </summary>
+        /// <returns>Контекст подключения к Redis</returns>
+        IConnectionMultiplexer CreateConnection();
+    }
+}
