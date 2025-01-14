db3ec56325c3ce75341b1605111efd0c796833ff
Александр <@USER>
2025-01-14 15:39:57 +0300

    Репозиторий событий #17

---
+++ Application/HostelFresh.Application.Abstractions/Repositories/IEventRepository.cs
@@ -0,0 +1,25 @@
+﻿using HostelFresh.Application.Abstractions.Entities;
+using HostelFresh.Shared.Enums;
+
+namespace HostelFresh.Application.Abstractions.Repositories
+{
+    public interface IEventRepository<TEntity> where TEntity : class, IEventEntity
+    {
+        /// <summary>
+        /// Создание события
+        /// </summary>
+        /// <param name="eventStream">Название потока <see cref="EventStreamTypes"/></param>
+        /// <param name="eventType">Тип события <see cref="EventTypes"/></param>
+        /// <param name="entity">Модель данных <typeparamref name="TEntity"/></param>
+        Task AddEvent(EventStreamTypes eventStream,  EventTypes eventType, TEntity entity);
+
+        /// <summary>
+        /// Получение списка событий
+        /// </summary>
+        /// <param name="eventStream">Название потока <see cref="EventStreamTypes"/></param>
+        /// <param name="type">Тип события <see cref="EventTypes"/></param>
+        /// <param name="id">ИД сущности</param>
+        /// <returns>Список событий</returns>
+        Task<IReadOnlyCollection<TEntity>> GetAllEvents(EventStreamTypes eventStream, EventTypes type, string id);
+    }
+}
---
+++ Shared/HostelFresh.Shared.Enums/EventStreamTypes.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HostelFresh.Shared.Enums
+{
+    /// <summary>
+    /// Типы потоков EventStore
+    /// </summary>
+    public enum EventStreamTypes
+    {
+        /// <summary>
+        /// Не указан
+        /// </summary>
+        [Display(Name = "Не указан")]
+        None = 0,
+
+        /// <summary>
+        /// Общежитие
+        /// </summary>
+        [Display(Name = "Общежитие")]
+        Hostel = 1,
+
+        /// <summary>
+        /// Пользователь
+        /// </summary>
+        [Display(Name = "Пользователь")]
+        User = 2,
+
+        /// <summary>
+        /// Жилец
+        /// </summary>
+        [Display (Name = "Жилец")]
+        Tenant = 3,
+
+        /// <summary>
+        /// Тарифный план
+        /// </summary>
+        [Display(Name = "Тарифные планы")]
+        Rate = 4
+    }
+}
