d294fefd6a08654b7f0c247b01bb743df45342f8
Александр <@USER>
2025-01-18 10:45:31 +0300

    Добавление AutoMapper и нескольких DTO

---
+++ Shared/HostelFresh.Shared.Common.Dtos/Mappers/UserMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using HostelFresh.Domain.Entities;
+using HostelFresh.Shared.Common.Dtos.Users;
+
+namespace HostelFresh.Shared.Common.Dtos.Mappers
+{
+    /// <summary>
+    /// Маппер пользователей
+    /// </summary>
+    public class UserMapper : Profile
+    {
+        public UserMapper()
+        {
+            /// User -> UserDto
+            CreateMap<User, UserDto>();
+
+            /// LoginUserDto -> User
+            CreateMap<LoginUserDto, User>();
+        }
+    }
+}
---
+++ Shared/HostelFresh.Shared.Common.Dtos/ResponseDto.cs
@@ -0,0 +1,24 @@
+﻿namespace HostelFresh.Shared.Common.Dtos
+{
+    /// <summary>
+    /// Базовый ответ сервера
+    /// </summary>
+    /// <typeparam name="T">Тип данных</typeparam>
+    public record ResponseDto<T> where T : class
+    {
+        /// <summary>
+        /// Статусный код ответа от сервера
+        /// </summary>
+        public int StatusCode { get; set; }
+
+        /// <summary>
+        /// Данные
+        /// </summary>
+        public T? Data { get; set; }
+
+        /// <summary>
+        /// Список ошибок
+        /// </summary>
+        public IReadOnlyCollection<string>? Errors { get; set; }
+    }
+}
